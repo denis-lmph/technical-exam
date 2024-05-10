ec7f4d73f510f42e750b833930b9fb5d80cfffc0
denis-lmph <denis.gutierrez@legalmatch.com>
2024-05-10 16:22:29 +0800

    refactor and styling fixes

--- src/js/script.js
+++ src/js/script.js
@@ -1,201 +1,246 @@
-window.ct = function(selector) {
+window.ct = function (selector) {
   const elements = document.querySelectorAll(selector);
 
   function createSearchInput(input, data) {
-      // add search container if it doesn't exist
-      if (!input.parentElement.classList.contains("search-container")) {
-          const container = document.createElement("div");
-          container.classList.add("search-container");
-          input.parentElement.insertBefore(container, input);
-          container.appendChild(input);
-      }
+    // add search container if it doesn't exist
+    if (!input.parentElement.classList.contains("search-container")) {
+      const container = document.createElement("div");
+      container.classList.add("search-container");
+      input.parentElement.insertBefore(container, input);
+      container.appendChild(input);
+    }
 
-      const container = input.parentElement;
-      // Create UL for search results
-      const ul = document.createElement("ul");
-      ul.classList.add("search-results");
-      container.appendChild(ul);
+    const container = input.parentElement;
+    // Create UL for search results
+    const ul = document.createElement("ul");
+    ul.classList.add("search-results");
+    container.appendChild(ul);
 
-      // Populate data as li elements
-      data.forEach(function(item) {
-          const li = document.createElement("li");
-          li.textContent = item.label;
-          ul.appendChild(li);
-      });
+    // Populate data as li elements
+    data.forEach(function (item) {
+      const li = document.createElement("li");
+      li.textContent = item.label;
+      ul.appendChild(li);
+    });
   }
 
-  elements.makeSearchInput = function({
-      data
-  }) {
-      this.forEach(function(input) {
-          createSearchInput(input, data);
+  function createModal(modal, content) {
+    // alert("hello 3 ");
+    // Create modal content
+    const modalContent = document.createElement("div");
+    modalContent.classList.add("modal-content");
 
-          // Show search results when input is not empty or focused
-          input.addEventListener("input", handleInput);
-          input.addEventListener("focus", handleInput);
+    // Close button
+    const closeButton = document.createElement("span");
+    closeButton.classList.add("close");
+    closeButton.innerHTML = "&times;";
 
-          function handleInput() {
-              const container = this.parentElement;
-              const ul = container.querySelector(".search-results");
-              const searchText = this.value.trim().toLowerCase();
-              // Filter data based on search text
-              const filteredData = data.filter(item =>
-                  item.label.toLowerCase().includes(searchText)
-              );
-              ul.innerHTML = "";
-              filteredData.forEach(function(item) {
-                  const li = document.createElement("li");
-                  const label = item.label;
-                  const startIndex = label.toLowerCase().indexOf(searchText);
+    closeButton.onclick = () => (modal.style.display = "none");
+    modalContent.appendChild(closeButton);
 
-                  if (startIndex !== -1) {
-                      const endIndex = startIndex + searchText.length;
-                      const highlightedText =
-                          label.substring(0, startIndex) +
-                          '<span class="highlight">' +
-                          label.substring(startIndex, endIndex) +
-                          "</span>" +
-                          label.substring(endIndex);
-                      li.innerHTML = highlightedText;
-                  } else {
-                      li.textContent = label;
-                  }
-                  ul.appendChild(li);
-              });
-              // Show or hide search results based on matching results
-              if (filteredData.length > 0 && searchText !== "") {
-                  ul.style.display = "block";
-              } else {
-                  ul.style.display = "none";
-              }
-          }
 
-          // Hide search results when input loses focus
-          input.addEventListener("blur", function() {
-              const container = this.parentElement;
-              const ul = container.querySelector(".search-results");
-              ul.style.display = "none";
-          });
+    // Title
+    // const titleElement = document.createElement("h2");
+    // titleElement.textContent = modal.getAttribute("data-title") || "";
+    // modalContent.appendChild(titleElement);
+
+    // Body
+    const bodyElement = document.createElement("div");
+    bodyElement.classList.add("contents");
+    bodyElement.innerHTML = modal.innerHTML;
+    modal.innerHTML = "";
+    modalContent.appendChild(bodyElement);
+
+    //Footer
+    const footerElement = document.createElement("div");
+    footerElement.classList.add("modal-footer");
+    const closeButtonFooter = document.createElement("button");
+    closeButtonFooter.id = "modalCloseBtn";
+    closeButtonFooter.textContent = "Close";
+    closeButtonFooter.addEventListener("click", () => {
+      modal.style.display = "none";
+    });
+    footerElement.appendChild(closeButtonFooter);
+    modalContent.appendChild(footerElement);
+
+    modal.appendChild(modalContent);
+  }
+
+  function convertSelectToDropdown(selectElement, { closeDropdownOnSelect }) {
+    const divDropdown = document.createElement("div");
+    divDropdown.classList.add("dropdown");
+
+    const button = document.createElement("button");
+    button.classList.add("dropdown-toggle");
+    // button.type = "button";
+    button.textContent = selectElement.getAttribute("data-label");
+
+    const spanCaret = document.createElement("span");
+    spanCaret.classList.add("caret");
+
+    const spanBadge = document.createElement("span");
+    spanBadge.classList.add("badge");
+    spanBadge.textContent = selectElement.getAttribute("data-badge");
+
+    const ulDropdownMenu = document.createElement("ul");
+    ulDropdownMenu.classList.add("dropdown-menu");
+
+    // Toggle dropdown menu on button click
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+      if (
+        ulDropdownMenu.style.display === "none" ||
+        ulDropdownMenu.style.display === ""
+      ) {
+        ulDropdownMenu.style.display = "block";
+      } else {
+        ulDropdownMenu.style.display = "none";
+      }
+    });
+
+    // Loop through each option in the select element
+    Array.from(selectElement.options).forEach(function (option) {
+      const liDropdownItem = document.createElement("li");
+      liDropdownItem.classList.add("dropdown-item");
+
+      // Clone all attributes of the option element and append them to the liDropdownItem
+      Array.from(option.attributes).forEach(function (attribute) {
+        liDropdownItem.setAttribute(attribute.nodeName, attribute.nodeValue);
       });
+
+      // Clone all child nodes of the option and append them to the liDropdownItem
+      Array.from(option.childNodes).forEach(function (childNode) {
+        liDropdownItem.appendChild(childNode.cloneNode(true));
+      });
+
+      if (closeDropdownOnSelect) {
+        liDropdownItem.addEventListener("click", function () {
+          ulDropdownMenu.style.display = "none";
+        });
+      }
+
+      ulDropdownMenu.appendChild(liDropdownItem);
+    });
+    // Append elements to form group div
+    divDropdown.appendChild(button);
+    divDropdown.appendChild(spanCaret);
+    divDropdown.appendChild(ulDropdownMenu);
+
+    // selectElement.remove();
+
+    // Replace select element with form group div
+    selectElement.parentNode.appendChild(divDropdown);
+    selectElement.parentNode.appendChild(spanBadge);
+    selectElement.remove();
+  }
+
+  elements.makeDropdown = function ({ closeDropdownOnSelect }) {
+    this.forEach(el => convertSelectToDropdown(el, { closeDropdownOnSelect }));
+  };
+
+  elements.makeModal = function () {
+    this.forEach(createModal);
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.addEventListener("click", function (event) {
+      elements.forEach(modal => {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      });
+    });
+
+    // Event listener to open each individual modal
+    const modalTriggers = document.querySelectorAll("[data-open-modal-id]");
+    modalTriggers.forEach(trigger => {
+      trigger.addEventListener("click", function (e) {
+        e.preventDefault();
+        const modalId = this.getAttribute("data-open-modal-id");
+        const modal = document.querySelector(`[data-modal-id="${modalId}"]`);
+        if (modal) {
+          modal.style.display = "block";
+        }
+      });
+    });
+  };
+
+  elements.makeSearchInput = function ({ data }) {
+    this.forEach(function (input) {
+      createSearchInput(input, data);
+
+      // Show search results when input is not empty or focused
+      input.addEventListener("input", handleInput);
+      input.addEventListener("focus", handleInput);
+
+      function handleInput() {
+        const container = this.parentElement;
+        const ul = container.querySelector(".search-results");
+        const searchText = this.value.trim().toLowerCase();
+        // Filter data based on search text
+        const filteredData = data.filter(item =>
+          item.label.toLowerCase().includes(searchText)
+        );
+        ul.innerHTML = "";
+        filteredData.forEach(function (item) {
+          const li = document.createElement("li");
+          const label = item.label;
+          const startIndex = label.toLowerCase().indexOf(searchText);
+
+          if (startIndex !== -1) {
+            const endIndex = startIndex + searchText.length;
+            const highlightedText =
+              label.substring(0, startIndex) +
+              '<span class="highlight">' +
+              label.substring(startIndex, endIndex) +
+              "</span>" +
+              label.substring(endIndex);
+            li.innerHTML = highlightedText;
+          } else {
+            li.textContent = label;
+          }
+          ul.appendChild(li);
+        });
+        // Show or hide search results based on matching results
+        if (filteredData.length > 0 && searchText !== "") {
+          ul.style.display = "block";
+        } else {
+          ul.style.display = "none";
+        }
+      }
+
+      // Hide search results when input loses focus
+      input.addEventListener("blur", function () {
+        const container = this.parentElement;
+        const ul = container.querySelector(".search-results");
+        ul.style.display = "none";
+      });
+    });
   };
 
   return elements;
 };
 
 // Initialize components when document is ready
-document.addEventListener("DOMContentLoaded", function() {
-  const data = [{
-          label: "Nowheresville, XX 00000"
-      },
-      {
-          label: "California, XX 12345"
-      },
-      {
-          label: "Fake Street, XX 68910"
-      },
-      {
-          label: "Queensland, XX 11111"
-      }
+document.addEventListener("DOMContentLoaded", function () {
+  const data = [
+    {
+      label: "Nowheresville, XX 00000"
+    },
+    {
+      label: "California, XX 12345"
+    },
+    {
+      label: "Fake Street, XX 68910"
+    },
+    {
+      label: "Queensland, XX 11111"
+    }
   ];
 
   ct(".search-input").makeSearchInput({
-      data
+    data
   });
 
-
-  // Get the modal
-  var modal = document.getElementById("myModal");
-
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
-
-  // Get the modal content elements
-  var modalContent1 = document.getElementById("modalContent1");
-  var modalContent2 = document.getElementById("modalContent2");
-  var modalContent3 = document.getElementById("modalContent3");
-
-  // Get the buttons
-  var category = document.getElementById("category");
-  var clickhere = document.getElementById("clickhere");
-  var reviewButton = document.querySelectorAll(".review-link");
-  var catitem = document.querySelectorAll(".dropdown-item");
-
-  // category.addEventListener("change", function () {
-  //   modalContent1.style.display = "block";
-  //   modalContent2.style.display = "none";
-  //   modalContent3.style.display = "none";
-  //   modal.style.display = "block";
-  // });
-
-
-
-  catitem.forEach(function(button) {
-      button.addEventListener("click", function() {
-          modalContent1.style.display = "block";
-          modalContent2.style.display = "none";
-          modalContent3.style.display = "none";
-          modal.style.display = "block";
-      });
-  });
-
-  reviewButton.forEach(function(button) {
-      button.addEventListener("click", function() {
-          modalContent1.style.display = "none";
-          modalContent2.style.display = "block";
-          modalContent3.style.display = "none";
-          modal.style.display = "block";
-      });
-  });
-
-  clickhere.addEventListener("click", function() {
-      modalContent1.style.display = "none";
-      modalContent2.style.display = "none";
-      modalContent3.style.display = "block";
-      modal.style.display = "block";
-  });
-
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-      modal.style.display = "none";
-  };
-
-  var btnclose = document.getElementById("modalCloseBtn");
-  btnclose.onclick = function() {
-      modal.style.display = "none";
-  };
-
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-      if (event.target == modal) {
-          modal.style.display = "none";
-      }
-  };
-
-
-
-
-  var dropdownToggle = document.querySelector('.dropdown-toggle');
-  var dropdownMenu = document.querySelector('.dropdown-menu');
-
-  dropdownToggle.addEventListener('click', function() {
-      event.preventDefault(); // Prevent default behavior
-      if (dropdownMenu.style.display === 'block') {
-
-
-          dropdownMenu.style.display = 'none';
-      } else {
-          dropdownMenu.style.display = 'block';
-      }
-  });
-
-  document.addEventListener('click', function(event) {
-      if (!dropdownToggle.contains(event.target) && !dropdownMenu.contains(event.target)) {
-          dropdownMenu.style.display = 'none';
-      }
-  });
-
-
-
-
+  ct(".select").makeDropdown({ closeDropdownOnSelect: true });
+  ct(".modal").makeModal();
 });
